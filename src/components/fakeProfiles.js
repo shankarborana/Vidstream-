aa48ae8f3254fde42b3954e60f042755c5ce6a9a
shankarborana <bhawanishankarborana@gmail.com>
2025-07-05 18:21:09 +0530

    Create fakeProfiles.js

---
+++ src/components/fakeProfiles.js
@@ -0,0 +1,46 @@
+import { countries } from "./countries";
+
+export const fakeProfiles = [
+  {
+    id: 1,
+    name: "Sana",
+    img: "/assets/girl-profiles/1.jpg",
+    country: countries[0]
+  },
+  {
+    id: 2,
+    name: "Emily",
+    img: "/assets/girl-profiles/2.jpg",
+    country: countries[1]
+  },
+  {
+    id: 3,
+    name: "Aiko",
+    img: "/assets/girl-profiles/3.jpg",
+    country: countries[2]
+  },
+  {
+    id: 4,
+    name: "Julia",
+    img: "/assets/girl-profiles/4.jpg",
+    country: countries[3]
+  },
+  {
+    id: 5,
+    name: "Maria",
+    img: "/assets/girl-profiles/5.jpg",
+    country: countries[4]
+  },
+  {
+    id: 6,
+    name: "Sophie",
+    img: "/assets/girl-profiles/6.jpg",
+    country: countries[5]
+  },
+  {
+    id: 7,
+    name: "Lena",
+    img: "/assets/girl-profiles/7.jpg",
+    country: countries[6]
+  }
+];
