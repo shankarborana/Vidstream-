643c71344559afba992bd3794a91fa1374e4c9ab
shankarborana <bhawanishankarborana@gmail.com>
2025-07-06 09:52:02 +0530

    Delete src directory

--- src/components/countries.js
+++
@@ -1,9 +0,0 @@
-export const countries = [
-  { name: "India", code: "IN", flag: "🇮🇳" },
-  { name: "United States", code: "US", flag: "🇺🇸" },
-  { name: "Japan", code: "JP", flag: "🇯🇵" },
-  { name: "Brazil", code: "BR", flag: "🇧🇷" },
-  { name: "Spain", code: "ES", flag: "🇪🇸" },
-  { name: "France", code: "FR", flag: "🇫🇷" },
-  { name: "Germany", code: "DE", flag: "🇩🇪" }
-];
